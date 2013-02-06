35b03722bbb99da6ad02064b666380f9fe4066f0
William D Arant <williamdarant@gmail.com>
2013-02-06 16:55:31 -0500

    changes

--- src/NewMain.java
+++ src/NewMain.java
@@ -4,6 +4,6 @@
  */
 class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello World!!! It's a beautiful day!"); // Display the string.
     }
 }
